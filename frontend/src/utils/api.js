ea46471c7a91309b05e15761f2ec75047e4eb35c
Ilya Sychugov <ilya.sychugov@gmail.com>
2021-06-18 18:49:38 +0300

    Fix: api url

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,58 +1,71 @@
-import {tokenAuth, groupId, apiURL} from '../utils/constants';
+import { apiURL } from '../utils/constants';
 
-export class Api{
-  constructor({tokenAuth, groupId, apiURL}){
-    this._apiURL = `${apiURL}/${groupId}`;
-    this._headers = { 
-      authorization: tokenAuth,
-      'Content-Type': 'application/json'
+export class Api {
+  constructor({ apiURL }) {
+    this._apiURL = apiURL;
+    this._headers = {
+      // authorization: tokenAuth,
+      'Content-Type': 'application/json',
     };
   }
 
-  getInitialCards(){
-    return fetch(`${this._apiURL}/cards`, {headers: this._headers})
-      .then(this._handleApiResult.bind(null, 'getInitialCards'))
+  getInitialCards() {
+    return fetch(`${this._apiURL}/cards`, { headers: this._headers }).then(
+      this._handleApiResult.bind(null, 'getInitialCards')
+    );
   }
 
-  addCard(cardData){
-    return fetch(`${this._apiURL}/cards`, {method: 'POST', headers: this._headers, body: JSON.stringify(cardData)})
-      .then(this._handleApiResult.bind(null, 'addCard'))
-  }
-  
-  toggleLikeCard(cardId, isLiked){
-    return fetch(`${this._apiURL}/cards/likes/${cardId}`, {method: isLiked ? 'DELETE' : 'PUT', headers: this._headers})
-      .then(this._handleApiResult.bind(null, 'likeCard'))
+  addCard(cardData) {
+    return fetch(`${this._apiURL}/cards`, {
+      method: 'POST',
+      headers: this._headers,
+      body: JSON.stringify(cardData),
+    }).then(this._handleApiResult.bind(null, 'addCard'));
   }
 
-  deleteCard(cardId){
-    return fetch(`${this._apiURL}/cards/${cardId}`, {method: 'DELETE', headers: this._headers,})
-      .then(this._handleApiResult.bind(null, 'deleteCard'))
+  toggleLikeCard(cardId, isLiked) {
+    return fetch(`${this._apiURL}/cards/likes/${cardId}`, {
+      method: isLiked ? 'DELETE' : 'PUT',
+      headers: this._headers,
+    }).then(this._handleApiResult.bind(null, 'likeCard'));
   }
 
-  editUserInfo(userData){
-    return fetch(`${this._apiURL}/users/me`, {method: 'PATCH', headers: this._headers, body: JSON.stringify(userData)})
-      .then(this._handleApiResult.bind(null, 'editUserInfo'))
-  } 
-
-  getUserInfo(){
-    return fetch(`${this._apiURL}/users/me`, {headers: this._headers})
-      .then(this._handleApiResult.bind(null, 'getUserInfo'))
+  deleteCard(cardId) {
+    return fetch(`${this._apiURL}/cards/${cardId}`, {
+      method: 'DELETE',
+      headers: this._headers,
+    }).then(this._handleApiResult.bind(null, 'deleteCard'));
   }
 
-  setUserAvatar(avatar){
-    return fetch(`${this._apiURL}/users/me/avatar`, {method: 'PATCH', headers: this._headers, body: JSON.stringify(avatar)})
-      .then(this._handleApiResult.bind(null, 'setUserAvatar'))
+  editUserInfo(userData) {
+    return fetch(`${this._apiURL}/users/me`, {
+      method: 'PATCH',
+      headers: this._headers,
+      body: JSON.stringify(userData),
+    }).then(this._handleApiResult.bind(null, 'editUserInfo'));
   }
 
-  _handleApiResult(fnName, res){
-    return res.ok ? res.json() : Promise.reject(`Ошибка получения результата в ${fnName}: ${res.status} ${res.statusText}`);
+  getUserInfo() {
+    return fetch(`${this._apiURL}/users/me`, { headers: this._headers }).then(
+      this._handleApiResult.bind(null, 'getUserInfo')
+    );
+  }
+
+  setUserAvatar(avatar) {
+    return fetch(`${this._apiURL}/users/me/avatar`, {
+      method: 'PATCH',
+      headers: this._headers,
+      body: JSON.stringify(avatar),
+    }).then(this._handleApiResult.bind(null, 'setUserAvatar'));
+  }
+
+  _handleApiResult(fnName, res) {
+    return res.ok
+      ? res.json()
+      : Promise.reject(`Ошибка получения результата в ${fnName}: ${res.status} ${res.statusText}`);
   }
 }
 
-const api = new Api({
-  tokenAuth: tokenAuth,
-  groupId: groupId,
-  apiURL: apiURL
-});
+const api = new Api({ apiURL: apiURL });
 
 export default api;
--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,8 +1,6 @@
 //api data
-export const tokenAuth = '30b18b22-df4e-456d-bb26-16cda3a69c12';
-export const groupId = 'cohort-21';
-export const apiURL = 'https://mesto.nomoreparties.co/v1';
-export const authURL = 'https://auth.nomoreparties.co';
+export const apiURL = 'https://api.ilya.nomoredomains.club';
+export const authURL = 'https://ilya.nomoredomains.club';
 
 export const signinURL = '/signin';
 export const signupURL = '/signup';
