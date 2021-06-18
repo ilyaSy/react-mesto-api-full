a15b1d0f2e3bc65b0723ec515e0cc1c1454d389d
Ilya Sychugov <ilya.sychugov@gmail.com>
2021-06-18 19:34:16 +0300

    Fix: auth url api

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,6 +1,6 @@
 //api data
 export const apiURL = 'https://api.ilya.nomoredomains.club';
-export const authURL = 'https://ilya.nomoredomains.club';
+export const authURL = 'https://api.ilya.nomoredomains.club';
 
 export const signinURL = '/signin';
 export const signupURL = '/signup';
